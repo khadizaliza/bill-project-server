78823969264637246ce6cb789f393ad49c588b59
khadizaliza <khadizaakter19836@gmail.com>
2022-07-02 18:59:09 +0600

    mongo-connet

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+var cors = require('cors')
+require('dotenv').config()
+app.use(cors())
+app.use(express.json)
 const port = process.env.PORT || 5000;
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.k2nsb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/', (req, res) =>{
     res.send('Hello from my sweet app')
